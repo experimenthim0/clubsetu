9fadd533ab7ddcb1a30c702f247321be09516673
Nikhil Yadav <contact.nikhim@gmail.com>
2026-02-17 08:52:46 +0530

    cors problem solved

--- server/index.js
+++ server/index.js
@@ -15,7 +15,7 @@ const PORT = process.env.PORT || 5000;
 
 const allowedOrigins = [
   "https://clubsetu.vercel.app",
-  "http://clubsetu.nikhim.me",
+  "https://clubsetu.nikhim.me",
   "http://localhost:5173",
   "http://localhost:5174",
   "http://localhost:5175",
